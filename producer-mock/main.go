09bb88ce79a6de5d842287ec0d1c17f93204fc4f
devv-monteiro <dev.monteiro.marcos@gmail.com>
2023-08-17 09:29:57 -0300

    made events sending being trigger by http request

--- producer-mock/main.go
+++ producer-mock/main.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"math/rand"
+	"net/http"
 	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
@@ -22,6 +23,39 @@ type PurchaseEvent struct {
 	Status              string
 }
 
+var sqsClient *sqs.SQS
+var queueUrl *string
+
+func sendEvent(resWr http.ResponseWriter, req *http.Request) {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	event := PurchaseEvent{
+		PurchaseId:          r.Intn(10000),
+		CreditAccountId:     123,
+		PurchaseDateTime:    time.Now().String(),
+		Amount:              float32(r.Intn(10000) / 100),
+		NumInstallments:     1,
+		MerchantDescription: "Acme Shop",
+		Status:              "APPROVED",
+	}
+	body, err := json.Marshal(event)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	output, err := sqsClient.SendMessage(&sqs.SendMessageInput{
+		MessageBody: aws.String(string(body)),
+		QueueUrl:    queueUrl,
+	})
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(output)
+	}
+
+	resWr.WriteHeader(200)
+}
+
 func main() {
 	time.Sleep(20 * time.Second)
 
@@ -37,43 +71,18 @@ func main() {
 		fmt.Println("Connected to SQS.")
 	}
 
-	sqsClient := sqs.New(sess)
-	queueName := "purchases-queue"
+	sqsClient = sqs.New(sess)
 
+	queueName := "purchases-queue"
 	queueUrlResp, err := sqsClient.GetQueueUrl(&sqs.GetQueueUrlInput{
 		QueueName: &queueName,
 	})
 	if err != nil {
 		fmt.Println(err)
 	}
+	queueUrl = queueUrlResp.QueueUrl
 
-	for i := 0; i < 50; i++ {
-		time.Sleep(2 * time.Second)
+	http.HandleFunc("/", sendEvent)
 
-		r := rand.New(rand.NewSource(time.Now().UnixNano()))
-		event := PurchaseEvent{
-			PurchaseId:          i,
-			CreditAccountId:     123,
-			PurchaseDateTime:    time.Now().String(),
-			Amount:              float32(r.Intn(10000) / 100),
-			NumInstallments:     1,
-			MerchantDescription: "Acme Shop",
-			Status:              "APPROVED",
-		}
-		body, err := json.Marshal(event)
-		if err != nil {
-			fmt.Println(err)
-		}
-
-		output, err := sqsClient.SendMessage(&sqs.SendMessageInput{
-			MessageBody: aws.String(string(body)),
-			QueueUrl:    queueUrlResp.QueueUrl,
-		})
-
-		if err != nil {
-			fmt.Println(err)
-		} else {
-			fmt.Println(output)
-		}
-	}
+	http.ListenAndServe(":80", nil)
 }
