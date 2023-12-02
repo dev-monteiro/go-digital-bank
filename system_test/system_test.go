ac30b11e452e7a4763de703f26d4ff9ba32bc813
devv-monteiro <dev.monteiro.marcos@gmail.com>
2023-12-02 10:20:18 -0300

    added cases to integration test

--- system_test/system_test.go
+++ system_test/system_test.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strconv"
 	"testing"
 	"time"
 
@@ -12,9 +13,7 @@ import (
 )
 
 type currInvoiceResp struct {
-	Status      string
-	Amount      string
-	ClosingDate string
+	Amount string
 }
 
 type purchaseReq struct {
@@ -22,54 +21,114 @@ type purchaseReq struct {
 	Amount float64
 }
 
-func TestSystem_NoAction(t *testing.T) {
-	invoResp, err := getCurrInvoice("abc-123-def")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	require.Equal(t, *invoResp, currInvoiceResp{
-		Status:      "Open",
-		Amount:      "$ 1234.50",
-		ClosingDate: "DEC 30",
-	})
+type coreBankInvoiceResp struct {
+	TotalAmount float64
 }
 
-func TestSystem_TriggerOnePurchase(t *testing.T) {
-	err := triggerPurchase(123, 11.11)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	time.Sleep(1 * time.Second)
-
-	invoResp, err := getCurrInvoice("abc-123-def")
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	require.Equal(t, *invoResp, currInvoiceResp{
-		Status:      "Open",
-		Amount:      "$ 1245.61",
-		ClosingDate: "DEC 30",
-	})
+type coreBankInvoiceListResp struct {
+	Content []coreBankInvoiceResp
 }
 
-func getCurrInvoice(custId string) (*currInvoiceResp, error) {
+func TestSystem(t *testing.T) {
+	fmt.Println("Init")
+
+	cbInvoResp := getCoreBankInvoice(t, 123)
+	require.Equal(t, *cbInvoResp, coreBankInvoiceResp{
+		TotalAmount: 1234.50,
+	})
+
+	invoResp := getCurrInvoice(t, "abc-123-def")
+	require.Equal(t, *invoResp, currInvoiceResp{
+		Amount: "$ 1234.50",
+	})
+
+	fmt.Println("OK")
+
+	fmt.Println("----------------------------")
+	fmt.Println("Trigger one purchase")
+
+	triggerPurchase(t, 123, 11.11)
+
+	cbInvoResp = getCoreBankInvoice(t, 123)
+	require.Equal(t, *cbInvoResp, coreBankInvoiceResp{
+		TotalAmount: 1234.50,
+	})
+
+	invoResp = getCurrInvoice(t, "abc-123-def")
+	require.Equal(t, *invoResp, currInvoiceResp{
+		Amount: "$ 1245.61",
+	})
+
+	fmt.Println("OK")
+
+	fmt.Println("----------------------------")
+	fmt.Println("Trigger multiple purchases")
+
+	triggerPurchase(t, 123, 22.22)
+	triggerPurchase(t, 123, 33.33)
+	triggerPurchase(t, 123, 44.44)
+
+	cbInvoResp = getCoreBankInvoice(t, 123)
+	require.Equal(t, *cbInvoResp, coreBankInvoiceResp{
+		TotalAmount: 1234.50,
+	})
+
+	invoResp = getCurrInvoice(t, "abc-123-def")
+	require.Equal(t, *invoResp, currInvoiceResp{
+		Amount: "$ 1345.60",
+	})
+
+	fmt.Println("OK")
+
+	fmt.Println("----------------------------")
+	fmt.Println("Trigger batch")
+
+	triggerBatch(t)
+
+	cbInvoResp = getCoreBankInvoice(t, 123)
+	require.Equal(t, *cbInvoResp, coreBankInvoiceResp{
+		TotalAmount: 1345.60,
+	})
+
+	invoResp = getCurrInvoice(t, "abc-123-def")
+	require.Equal(t, *invoResp, currInvoiceResp{
+		Amount: "$ 1345.60",
+	})
+
+	fmt.Println("OK")
+}
+
+func getCoreBankInvoice(t *testing.T, cbCustId int) *coreBankInvoiceResp {
+	url := "http://localhost:9090/invoices?creditAccountId=" + strconv.Itoa(cbCustId)
+
+	httpResp, err := http.Get(url)
+	if err != nil {
+		onError(t, err)
+		return nil
+	}
+
+	var cbInvoListResp coreBankInvoiceListResp
+	json.NewDecoder(httpResp.Body).Decode(&cbInvoListResp)
+
+	return &cbInvoListResp.Content[0]
+}
+
+func getCurrInvoice(t *testing.T, custId string) *currInvoiceResp {
 	url := "http://localhost:8080/invoices/current?customerId=" + custId
 
 	httpResp, err := http.Get(url)
 	if err != nil {
-		return nil, err
+		onError(t, err)
+		return nil
 	}
 
 	var invoResp currInvoiceResp
 	json.NewDecoder(httpResp.Body).Decode(&invoResp)
 
-	return &invoResp, nil
+	return &invoResp
 }
 
-func triggerPurchase(cbCustId int, amount float64) error {
+func triggerPurchase(t *testing.T, cbCustId int, amount float64) {
 	url := "http://localhost:9090/trigger/purchase"
 
 	payload, err := json.Marshal(purchaseReq{
@@ -77,13 +136,32 @@ func triggerPurchase(cbCustId int, amount float64) error {
 		Amount: amount,
 	})
 	if err != nil {
-		return err
+		onError(t, err)
+		return
 	}
 
 	_, err = http.Post(url, "application/json", bytes.NewBuffer(payload))
 	if err != nil {
-		return err
+		onError(t, err)
+		return
 	}
 
-	return nil
+	time.Sleep(1 * time.Second)
+}
+
+func triggerBatch(t *testing.T) {
+	url := "http://localhost:9090/trigger/batch"
+
+	_, err := http.Get(url)
+	if err != nil {
+		onError(t, err)
+		return
+	}
+
+	time.Sleep(1 * time.Second)
+}
+
+func onError(t *testing.T, err error) {
+	fmt.Println(err)
+	t.FailNow()
 }
